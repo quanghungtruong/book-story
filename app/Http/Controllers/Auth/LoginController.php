6915c08b4399a68fe71eae01ddcd847ff1b4e2f5
Hung Truong <tquanghung@tma.com.vn>
2018-10-03 15:58:06 +0700

    make loging page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,14 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function showLogin()
+    {
+        return view('admin.login');
+    }
+
+    public function doLogin()
+    {
+        
+    }
 }
